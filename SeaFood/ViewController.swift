8d61380752fe055166e7a0c069121de7a8ef84aa
Junsoo Park <matthiaspark0514@gmail.com>
2022-03-24 16:40:32 -0700

    Getting Image Recognition Results Back from Our V3 Mode

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -33,11 +33,35 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("We could't convert UIimage to CIImage")
+            }
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading coreML model failed.")
+        }
         
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results  = request.results as? [VNClassificationObservation] else {
+                fatalError("Model Failed Process Image")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
