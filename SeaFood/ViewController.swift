54de83c4af90a271b28f2317510d804211b6eb6c
Junsoo Park <matthiaspark0514@gmail.com>
2022-03-25 20:06:50 -0700

    real determine a photo as a "Hot dog" or "Not Hot dog"

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -52,7 +52,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let results  = request.results as? [VNClassificationObservation] else {
                 fatalError("Model Failed Process Image")
             }
-            print(results)
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
